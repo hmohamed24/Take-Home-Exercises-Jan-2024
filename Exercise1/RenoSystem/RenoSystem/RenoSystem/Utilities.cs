f99bb8df2db5141c029136a1efc50b33a74d7590
hmohamed24 <90639938+hmohamed24@users.noreply.github.com>
2024-01-24 21:58:45 -0700

    updated solution

---
+++ Exercise1/RenoSystem/RenoSystem/RenoSystem/Utilities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenoSystem
+{
+	public static class Utilities
+	{
+		public static bool IsNonZeroPositive(int numeric)
+		{
+			return numeric > 0;
+		}
+
+		public static bool IsZeroPositive(int numeric)
+		{
+			return numeric >= 0;
+		}
+
+		public static bool IsValidMinimum(int numeric, int minimum)
+		{
+			return numeric >= minimum;
+		}
+	}
+}
