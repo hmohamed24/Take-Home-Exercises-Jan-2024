b53b6e8e8e04892172b1179cffa868ccc1ba8608
hmohamed24 <90639938+hmohamed24@users.noreply.github.com>
2024-02-08 20:59:47 -0700

    Final Update

---
+++ Exercise2/RenoSystem/RenoSystem/RenoSystem/Opening.cs
@@ -0,0 +1,76 @@
+﻿namespace RenoSystem
+{
+	public class Opening
+	{
+		private int _edging = 0;
+		private int _height = 0;
+		private int _width = 0;
+
+		public int Edging
+		{
+			get { return _edging; }
+			set
+			{
+				if (value != 0 && !Utilities.IsValidMinimum(value, 10))
+				{
+					throw new ArgumentException("The minimum edging is 10 cm.");
+				}
+
+				_edging = value;
+			}
+		}
+
+		public int Height
+		{
+			get { return _height; }
+			set
+			{
+				if (!Utilities.IsValidMinimum(value, 120))
+				{
+					throw new ArgumentException("The minimum height is 120 cm.");
+				}
+
+				_height = value;
+			}
+		}
+
+		public int Width
+		{
+			get { return _width; }
+			set
+			{
+				if (!Utilities.IsValidMinimum(value, 50))
+				{
+					throw new ArgumentException("The minimum width is 50 cm.");
+				}
+
+				_width = value;
+			}
+		}
+
+		public int Area
+		{
+			get { return Width * Height; }
+		}
+
+		public int Perimeter
+		{
+			get { return (Width + Height) * 2; }
+		}
+
+		public OpeningType Type { get; set; }		
+
+		public Opening(int width, int height, OpeningType type, int edging = 0)
+		{
+			Width = width;
+			Height = height;
+			Type = type;
+			Edging = edging;
+		}
+
+		public string ToString()
+		{
+			return $"{Width},{Height},{Type},{Edging}";
+		}
+	}
+}
---
+++ Exercise2/RenoSystem/RenoSystem/RenoSystem/Room.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenoSystem
+{
+	public class Room
+	{
+		private string _Name;
+
+		public string Name
+		{
+			get
+			{
+				return _Name;
+			}
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value.Trim()))
+				{
+					throw new ArgumentException("Name is missing! Please enter a Name to proceed.");
+				}
+
+				_Name = value;
+			}
+		}
+
+		public string Flooring { get; set; }
+
+		public List<Wall> Walls { get; private set; } = new();
+
+		public int TotalWalls
+		{
+			get
+			{
+				return Walls.Count;
+			}
+			set { }
+		}
+
+		public Room(string name, string flooring, List<Wall> walls)
+		{
+			Name = name;
+			Flooring = flooring;
+			Walls = walls;
+		}
+
+		public void AddWall(Wall wall)
+		{
+			if (wall == null)
+			{
+				throw new ArgumentException("Wall information is missing! Please enter the information of the Wall to proceed.");
+			}
+			else
+			{
+				bool planIdExists = false;
+
+				foreach (Wall w in Walls)
+				{
+					if (w.PlanId == wall.PlanId)
+					{
+						planIdExists = true;
+					}
+				}
+
+				if (planIdExists)
+				{
+					throw new ArgumentException("PlanId already exists.");
+				}
+				else
+				{
+					Walls.Add(wall);
+				}
+			}
+		}
+
+		public void RemoveWall(string planId)
+		{
+			if (string.IsNullOrWhiteSpace(planId))
+			{
+				throw new ArgumentException("PlanId is missing! Please enter a PlanId to proceed.");
+			}
+			else
+			{
+				foreach (Wall wall in Walls)
+				{
+					if (wall.PlanId == planId)
+					{
+						Walls.Remove(wall);
+					}
+				}
+			}
+		}
+	}
+}
